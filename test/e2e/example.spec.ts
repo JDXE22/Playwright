f2c97b858cc4282b5ecba650c089ec5a1eb50c31
David <davidesparzaj22@gmail.com>
2025-08-27 18:48:19 -0500

    test(test/e2e/example.spec.ts):
    replace default example with end-to-end navigation
    
    test for FreeRangers site

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,6 +1,26 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Browser, Page } from "@playwright/test";
 
-test("Homepage loads and renders the title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  await expect(page).toHaveTitle(/Playwright/);
-});
+(async () => {
+  let browser: Browser;
+  let page: Page;
+
+  test.describe("Navigation on FreeRangers", () => {
+    test("Main links redirect correctly", async ({ page }) => {
+      await test.step("Given I go to the home page", async () => {
+        page.goto("https://www.freerangetesters.com");
+        await expect(page).toHaveTitle(/Free Range Testers/);
+      });
+      await test.step("When I click on the courses link", async () => {
+        page
+          .locator("#page_header")
+          .getByRole("link", { name: "Cursos", exact: true })
+          .click();
+        await page.waitForURL("**/cursos");
+      });
+
+      await test.step('Then I should see the "Cursos" page', async () => {
+        await expect(page).toHaveTitle(/Cursos/);
+      });
+    });
+  });
+})();
